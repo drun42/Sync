e72d28e9c8cda7ed4e1e8c7d91a8eb2383c36760
Ubu <ritoutoudu24@gmail.com>
2026-07-16 01:16:42 +0200

    auto

---
+++ C03/ex00/ft_strcmp.c
@@ -0,0 +1,22 @@
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	/* compare *s1 et *s2
+	 * if s1 = s2 return 0
+	 * if s1 < s2 return -
+	 * if s1 > s2 return +
+	 * when it says that it compare it compare not the length but the ascii
+	 * value of each character in the string*/
+
+	int	as1;
+	int	counts1;
+	int	as2;
+	int	counts2;
+	int	i;
+	
+	while(s1[i])
+	{
+
+		
+
+}
