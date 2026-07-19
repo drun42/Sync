1fc5895988359fa21c1cdae2e47e6d4385d16a7e
Dmitry Shofer <dshofer@c1r3p6.42nice.fr>
2026-07-19 16:46:32 +0200

    auto

---
+++ C03/ex02/ft_strcat.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dshofer <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/19 14:07:16 by dshofer           #+#    #+#             */
+/*   Updated: 2026/07/19 14:42:20 by dshofer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (dest[i])
+	{
+		i++;
+	}
+	while (src[j])
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+/*
+int main(void)
+{
+    char dest[52] = "string";
+    char src[] = "completed";
+    //char *dest = &s1;
+    //char *src = s2;
+    printf("%s", ft_strcat(dest, src));
+}
+    */
