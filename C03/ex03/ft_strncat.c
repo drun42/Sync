baf160788fcd63c9901e7064158256a03947b4c6
Dmitry Shofer <dshofer@c1r4p2.42nice.fr>
2026-07-20 10:29:27 +0200

    auto

--- C03/ex03/ft_strncat.c
+++ C03/ex03/ft_strncat.c
@@ -6,28 +6,47 @@
 /*   By: dshofer <dshofer@student.42nice.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/19 14:43:25 by dshofer           #+#    #+#             */
-/*   Updated: 2026/07/20 09:11:06 by dshofer          ###   ########.fr       */
+/*   Updated: 2026/07/20 10:28:24 by dshofer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stdio.h>
 
-
-char	*ft_strncat(char *dest, char *src, unsigned int nb);
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
 {
-    int i;
-    int j;
+	int i;
+	unsigned int j;
 
-    i = 0;
-    j = 0;
-    while (dest[i])
-    {
-        i++;
-    }
-    while (j < n)
-    {
-        dest[i] = src[j];
-        i++;
-        j++;
-    }
-    dest[i] = '\0';
-    return(dest);
+	i = 0;
+	j = 0;
+	 while (dest[i])
+	{
+		i++;
+	}
+	while (src[j])
+	{
+		while (j < nb)
+		{
+			dest[i] = src[j];
+			i++;
+			j++;
+		}
+		break;
+	}
+	dest[i] = '\0';
+	return(dest);
 }
+/*
+int	main(void)
+{
+	char dest[225] = "pizda ";
+	char src[] = "dyryavaya";
+	unsigned int nb;
+
+	nb = 3;	
+	printf("%s\n",ft_strncat(dest, src, 3));
+	printf("%s\n",ft_strncat(dest, src, 9));
+	printf("%s\n",ft_strncat(dest, src, 35));
+	printf("%s\n",ft_strncat(dest, src, 0));
+
+}
+*/
