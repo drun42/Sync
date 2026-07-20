112176567c17f8aff88f94ff89e8ab412255c024
Dmitry Shofer <dshofer@c1r4p2.42nice.fr>
2026-07-20 09:12:05 +0200

    auto

--- C03/ex03/ft_strncat.c
+++ C03/ex03/ft_strncat.c
@@ -6,8 +6,28 @@
 /*   By: dshofer <dshofer@student.42nice.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/19 14:43:25 by dshofer           #+#    #+#             */
-/*   Updated: 2026/07/19 14:43:33 by dshofer          ###   ########.fr       */
+/*   Updated: 2026/07/20 09:11:06 by dshofer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
+char	*ft_strncat(char *dest, char *src, unsigned int nb);
+{
+    int i;
+    int j;
+
+    i = 0;
+    j = 0;
+    while (dest[i])
+    {
+        i++;
+    }
+    while (j < n)
+    {
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
+    dest[i] = '\0';
+    return(dest);
+}
