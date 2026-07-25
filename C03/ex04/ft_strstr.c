a71f2656db7cb7fb61a915c6c28c0ec40afb50a7
Ubu <ritoutoudu24@gmail.com>
2026-07-26 01:03:54 +0200

    auto

--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -1,48 +1,46 @@
-
-int needle_check(char)
-{
-	
-}
+#include <stdio.h>
 
 char *ft_strstr(char *str, char *to_find)
 {
-	//finds the first occurrence of the substring needle in the string haystack.
-	int	i;
-	int	i2;
-	int	temp;
+    int i;
+    int j;
 
-	i = 0;
-	i2 = 0;
-	while(haystack[i])
-	{
-		if(haystack[i] == needle[i2])
-		{
-			temp = i;
-			while(needle[i2])
-			{
-				if (needle[i2] == haystack[i])
-
-			}
-		}
-			
-	}
-
-	//The terminating null bytes ('\0') are  not compared.
-
-	
-	//return a pointer to the beginning of the  located  substring, 
-	return (haystack[i]);
-	//or NULL if the substring is not found.
-	
-	//If  needle is the empty string, the return value is always haystack itself.
-	if (needle[0] = '\0')
-		return(haystack);
+    if (to_find[0] == '\0')
+        return str;
+    
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] == to_find[0])
+        {
+            j = 0;
+            while (to_find[j] != '\0' && str[i + j] == to_find[j])
+                j++;
+            if (to_find[j] == '\0')
+                return &str[i];
+        }
+    }
+    return 0;
 }
 
-int	main(void)
+int main(void)
 {
-	char	needle[] = "help";
-	char	haystack[] = "please help me";
+    char haystack[] = "please help me";
+    char *result;
 
+    result = ft_strstr(haystack, "help");
+    printf("search 'help' -> %s\n", result ? result : "(null)");
 
+    result = ft_strstr(haystack, "please");
+    printf("search 'please' -> %s\n", result ? result : "(null)");
+
+    result = ft_strstr(haystack, "me");
+    printf("search 'me' -> %s\n", result ? result : "(null)");
+
+    result = ft_strstr(haystack, "xyz");
+    printf("search 'xyz' -> %s\n", result ? result : "(null)");
+
+    result = ft_strstr(haystack, "");
+    printf("search '' -> %s\n", result ? result : "(null)");
+
+    return 0;
 }
