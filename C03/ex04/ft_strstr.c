857d0c41033719a77301d4bbff185737ca9a2b17
Dmitry Shofer <dshofer@c1r4p8.42nice.fr>
2026-07-26 02:04:13 +0200

    auto

--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -1,46 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dshofer <dshofer@student.42nice.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/26 01:49:58 by dshofer           #+#    #+#             */
+/*   Updated: 2026/07/26 01:58:48 by dshofer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 
-char *ft_strstr(char *str, char *to_find)
+char	*ft_strstr(char *str, char *to_find)
 {
-    int i;
-    int j;
+	int	i;
+	int	j;
 
-    if (to_find[0] == '\0')
-        return str;
-    
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        if (str[i] == to_find[0])
-        {
-            j = 0;
-            while (to_find[j] != '\0' && str[i + j] == to_find[j])
-                j++;
-            if (to_find[j] == '\0')
-                return &str[i];
-        }
-    }
-    return 0;
+	if (to_find[0] == '\0')
+		return (str);
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == to_find[0])
+		{
+			j = 0;
+			while (to_find[j] != '\0' && str[i + j] == to_find[j])
+			{
+				j++;
+				if (to_find[j] == '\0')
+					return (&str[i]);
+			}
+		}
+		i++;
+	}
+	return (0);
 }
-
+/*
 int main(void)
 {
     char haystack[] = "please help me";
-    char *result;
 
-    result = ft_strstr(haystack, "help");
-    printf("search 'help' -> %s\n", result ? result : "(null)");
-
-    result = ft_strstr(haystack, "please");
-    printf("search 'please' -> %s\n", result ? result : "(null)");
-
-    result = ft_strstr(haystack, "me");
-    printf("search 'me' -> %s\n", result ? result : "(null)");
-
-    result = ft_strstr(haystack, "xyz");
-    printf("search 'xyz' -> %s\n", result ? result : "(null)");
-
-    result = ft_strstr(haystack, "");
-    printf("search '' -> %s\n", result ? result : "(null)");
-
-    return 0;
+    printf("%s\n", ft_strstr(haystack, "helpe"))
 }
+*/
