0f32d707534193264b8cf570d04d359bd9880c3d
Ubu <ritoutoudu24@gmail.com>
2026-07-27 05:55:53 +0200

    auto

--- C05/ex04/ft_fibonacci.c
+++ C05/ex04/ft_fibonacci.c
@@ -6,13 +6,25 @@
 /*   By: us <marvin@42.fr>                          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/27 04:52:23 by us                #+#    #+#             */
-/*   Updated: 2026/07/27 05:17:55 by us               ###   ########.fr       */
+/*   Updated: 2026/07/27 05:49:56 by us               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 int ft_fibonacci(int index)
-{
+{	
+	int	res;
+
+
 	if (index < 0)
 		return (-1);
+	if (index == 1)
+		return (1);
+	return (res = ft_fibonacci(index -1) + ft_fibonacci(index -1));
+}
 
+int main (void)
+{
+	printf("%d",ft_fibonacci(4));
 }
